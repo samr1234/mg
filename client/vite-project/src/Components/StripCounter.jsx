e3ab9df19c87af33bec77b690803cf1d97b9f1a2
samridh <samridhpctebtech19cse@gmail.com>
2023-05-17 11:10:27 +0530

    updated

--- client/vite-project/src/Components/StripCounter.jsx
+++ client/vite-project/src/Components/StripCounter.jsx
@@ -16,30 +16,23 @@ const StripCounter = () => {
     <div className="sm:flex block mx-auto w-[20rem]  justify-around  mt-12 pb-12 ">
       
       <div className=" border-8 shadow-lg shadow-indigo-500/50 border-indigo-500/50 rounded-full py-12 px-12 text-2xl bg-gradient-to-r ">
-        {/* <CountUp start={0} end={250} duration={4}>
+        <CountUp start={0} end={250} duration={4}>
           {({ countUpRef, start }) => (
             <div className="flex flex-col items-center">
-              <span className="text-indigo-500/50 font-bold" ref={countUpRef}></span>
+              <span className="text-indigo-500/50 font-bold" ref={countUpRef}>250</span>
               <button className=""onClick={start}>
            <span className="text-indigo-500/50 font-bold">Projects</span> 
               </button>
             </div>
           )}
-        </CountUp> */}
-        {
-   <div className="flex flex-col items-center">
-  <span id="counter" />
-  <h2>Projects</h2>
-   </div>
-  
-        }
+        </CountUp>
     
       </div>
       <div className="border-8 shadow-lg border-cyan-500/50 shadow-cyan-500/50 rounded-full py-12 px-12 text-2xl">
         <CountUp start={0} end={300} duration={4}>
           {({ countUpRef, start }) => (
             <div className="flex flex-col items-center">
-              <span className="text-cyan-500/50 font-bold" ref={countUpRef} />
+              <span className="text-cyan-500/50 font-bold" ref={countUpRef}>300</span>
               <button onClick={start}>
                 <span className="text-cyan-500/50 font-bold">Clients</span>
               </button>
@@ -51,7 +44,7 @@ const StripCounter = () => {
         <CountUp start={0} end={50} duration={4}>
           {({ countUpRef, start }) => (
             <div className="flex flex-col items-center">
-              <span className="text-[#74F2CE] font-bold" ref={countUpRef} />
+              <span className="text-[#74F2CE] font-bold" ref={countUpRef}>50</span>
               <button onClick={start}>
                 <span className="text-[#74F2CE] font-bold"> Teams </span>
               </button>
@@ -63,7 +56,7 @@ const StripCounter = () => {
         <CountUp start={0} end={15} duration={4}>
           {({ countUpRef, start }) => (
             <div className="flex flex-col items-center">
-              <span className="text-[#F7B42C] font-bold" ref={countUpRef} />
+              <span className="text-[#F7B42C] font-bold" ref={countUpRef}>15</span>
               <button onClick={start}>
                <span className="text-[#F7B42C] font-bold">Awards</span> 
               </button>
